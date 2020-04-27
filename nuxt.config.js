fc371544144d1d9f1dd8334d81eb19dd6a6c85f4
Junya Oiwa <0920oiwa@gmail.com>
2020-04-28 02:04:49 +0900

    [update]余分なコメントを削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,9 +2,6 @@ require("dotenv").config();
 const { API_KEY } = process.env;
 export default {
   mode: "spa",
-  /*
-   ** Headers of the page
-   */
   head: {
     title: process.env.npm_package_name || "",
     meta: [
@@ -18,33 +15,12 @@ export default {
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
-  /*
-   ** Customize the progress-bar color
-   */
   loading: { color: "#fff" },
-  /*
-   ** Global CSS
-   */
   css: ["@/assets/style/reset.css"],
-  /*
-   ** Plugins to load before mounting the App
-   */
   plugins: ["~/plugins/markdownit"],
-  /*
-   ** Nuxt.js dev-modules
-   */
   buildModules: [],
-  /*
-   ** Nuxt.js modules
-   */
   modules: [],
-  /*
-   ** Build configuration
-   */
   build: {
-    /*
-     ** You can extend webpack config here
-     */
     analyze: true,
     extend(config, ctx) {}
   },
@@ -54,11 +30,11 @@ export default {
   ],
   axios: {},
   markdownit: {
-    injected: true, // $mdを利用してmarkdownをhtmlにレンダリングする
-    breaks: true, // 改行コードを<br>に変換する
-    html: true, // HTML タグを有効にする
-    linkify: true, // URLに似たテキストをリンクに自動変換する
-    typography: true // 言語に依存しないきれいな 置換 + 引用符 を有効にします。
+    injected: true,
+    breaks: true,
+    html: true,
+    linkify: true,
+    typography: true
   },
   bootstrapVue: {
     components: [
